c38696f4bab61c2a34a0c2c903927be6a462d947
Jean Alves <jean.donizete.alves@gmail.com>
2024-11-23 22:53:05 -0300

    feat: adiciona opcao para acessar sala via codigo

--- src/app/shared/components/room-form/room-form.component.ts
+++ src/app/shared/components/room-form/room-form.component.ts
@@ -1,21 +1,45 @@
 import { Component, inject, output } from '@angular/core';
 import { PlayerService } from '../../services/player/player.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './room-form.component.html',
   styleUrl: './room-form.component.scss',
 })
 export class RoomFormComponent {
   submit$ = output<string>();
 
+  roomForm: FormGroup;
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
   private playerService = inject(PlayerService);
+
+  constructor() {
+    this.roomForm = this.fb.group({
+      idRoom: ['', Validators.required],
+    });
+  }
 
   createRoom(): void {
     this.playerService.createRoom().subscribe((idRoom) => {
       this.submit$.emit(idRoom);
     });
+  }
+
+  submitHandler(form: FormGroup): void {
+    if (form.invalid) {
+      return;
+    }
+    this.router.navigateByUrl(`/sala/${form.value.idRoom}`);
   }
 }
