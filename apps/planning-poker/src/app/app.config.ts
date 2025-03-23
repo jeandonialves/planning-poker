633505983650192bddd44bea9d062103f6047892
Jean Alves <jean.donizete.alves@gmail.com>
2025-03-23 00:29:03 -0300

    ✨ feat: add ng-zorro

--- apps/planning-poker/src/app/app.config.ts
+++ apps/planning-poker/src/app/app.config.ts
@@ -1,10 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import pt from '@angular/common/locales/pt';
+
 import { appRoutes } from './app.routes';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideNzI18n(pt_BR),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
