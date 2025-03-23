29940d9ef047904247a42120add487356d558335
Jean Alves <jean.donizete.alves@gmail.com>
2025-03-23 00:18:50 -0300

    🎉 initial: create new structured

--- src/app/shared/pipe/filter-hook.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterHookPipe } from './filter-hook.pipe';
-
-describe('FilterHookPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterHookPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/tokens/index.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const RESPONSE = new InjectionToken<Response>('RESPONSE');
-export const LOCAL_STORAGE = new InjectionToken<Storage>('localStorage');
